6f8bea5cc4dc456a755a46b0d8a93898ed53378a
fantarqse <fantarose51@gmail.com>
2023-09-19 21:24:41 +0300

    test: added tests for tx money transfer

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,91 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	ctx := context.Background()
+
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// Run n current transfer transactions.
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			// Converts int64 to sql.NullInt64
+			var fromAccountID sql.NullInt64
+			fromAccountID.Int64 = account1.ID
+			fromAccountID.Valid = true
+
+			var toAccountID sql.NullInt64
+			toAccountID.Int64 = account2.ID
+			toAccountID.Valid = true
+
+			result, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	// Checks results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		// Checks transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID.Int64)
+		require.Equal(t, account2.ID, transfer.ToAccountID.Int64)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(ctx, transfer.ID)
+		require.NoError(t, err)
+
+		// Checks entries
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID.Int64)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(ctx, fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID.Int64)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(ctx, toEntry.ID)
+		require.NoError(t, err)
+
+		// TODO: check accounts' balance
+	}
+}
